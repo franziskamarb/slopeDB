14cd1176dced1f6d19c86cce7a52136fa7ecc2b5
Jan Mühlnikel <jan.muehlnikel@gizonline.onmicrosoft.com>
2023-05-14 17:33:00 +0200

    Test

---
+++ Insert_Students.sql
@@ -0,0 +1,10 @@
+INSERT INTO public."STUDENT"(
+	"Student_id", "First_Name", "Last_Name", "Birthdate", "Adult", "Course_type", "Experience_level", "Phonenumber", "Sex", "Street", "House_num", "Post-code", "Accomodation", "Skier", "Ski_suit", "Ski_pole")
+	VALUES ('S1', 'Jan', 'Mühlnikel', '02.08.2001', 'True', 'Anfänger', 'Niedrig', '0160 432554322', 'Männlich', 'Straße', '1', '68767', 'A1', 'Skier1', 'Suit1', 'Poles1');
+	
+INSERT INTO public."STUDENT"(
+	"Student_id", "First_Name", "Last_Name", "Birthdate", "Adult", "Course_type", "Experience_level", "Phonenumber", "Sex", "Street", "House_num", "Post-code", "Accomodation", "Skier", "Ski_suit", "Ski_pole")
+	VALUES ('S2', 'Jan2', 'Mühlnikel', '02.08.2001', 'True', 'Anfänger', 'Niedrig', '0160 432554322', 'Männlich', 'Straße', '1', '68767', 'A1', 'Skier1', 'Suit1', 'Poles1');
+	
+	
+	
