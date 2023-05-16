85ffd9b12bf8dffabaadc4d3f251b7dbfb536965
franziskamarb <96066473+franziskamarb@users.noreply.github.com>
2023-05-16 16:01:31 +0200

    Update Insert_Employee.sql

--- sql/insert_data/Insert_Employee.sql
+++ sql/insert_data/Insert_Employee.sql
@@ -1,3 +1,17 @@
 INSERT INTO public."EMPLOYEE"(
 	"Employee_id", "First_Name", "Last_Name", "Job_title", "Salary_group", "Instructor_level", "Street", "House_num", "Post_code", "Birthdate", "Age", "Sex", "Phonenumber")
-	VALUES ('E1', 'Test', 'Employee', 'Ski Lehrer', 'A', 'L1', 'Street', '1a', '35524', '02-08-2001', '21', 'Männlich', '1040 2324241');
+	VALUES ('E1', 'Ludwig', 'Maier', 'Ski School Director', 'O', 'DSV Diplom-Instructor', 'Hauptstraße', '1', '87561', '1985-05-10', 36, 'M', '123456789')
+		  ,('E2', 'Anna', 'Huber', 'Ski School Director', 'P', 'DSV Diplom-Instructor', 'Dorfstraße', '5', '87561', '1988-08-20', 33, 'F', '987654321')
+		  ,('E3', 'Simon', 'Schneider', 'Ski Instructor', 'K', 'DSV Diplom-Instructor', 'Bergweg', '10', '87561', '1990-03-15', 31, 'M', '456123789')
+		  ,('E4', 'Monika', 'Müller', 'Ski Instructor', 'K', 'DSV Diplom-Instructor', 'Kirchplatz', '3', '87561', '1987-11-27', 34, 'F', '789123456')
+		  ,('E5', 'Sebastian', 'Bauer', 'Ski Instructor', 'J', 'DSV Landes-Instructor', 'Bergstraße', '8', '87561', '1989-06-05', 32, 'M', '654789123')
+		  ,('E6', 'Sabine', 'Schmitt', 'Ski Instructor', 'J', 'DSV Landes-Instructor', 'Feldweg', '12', '87561', '1992-09-15', 29, 'F', '321987654')
+		  ,('E7', 'Michael', 'Fischer', 'Ski Instructor', 'J', 'DSV Landes-Instructor', 'Wiesenweg', '6', '87561', '1986-02-18', 35, 'M', '987321654')
+		  ,('E8', 'Julia', 'Weber', 'Ski Instructor', 'I', 'DSV Snowsport-Instructor', 'Bahnhofstraße', '2', '87561', '1991-07-12', 30, 'F', '456789123')
+		  ,('E9', 'Andreas', 'Mayer', 'Ski Instructor', 'I', 'DSV Snowsport-Instructor', 'Sonnenweg', '9', '87561', '1984-12-09', 37, 'M', '789654321')
+		  ,('E10', 'Stefanie', 'Schmid', 'Ski Instructor', 'G', 'DSV Basis-Instructor', 'Birkenallee', '4', '87561', '1993-04-25', 28, 'F', '321456987')
+		  ,('E11', 'Thomas', 'Bauer', 'Assistant Ski Instructor', 'G', 'DSV Basis-Instructor', 'Rosenstraße', '7', '87561', '1986-10-08', 35, 'M', '654321987')
+		  ,('E12', 'Christine', 'Müller', 'Assistant Ski Instructor', 'G', 'DSV Basis-Instructor', 'Am Anger', '11', '87561', '1989-03-21', 32, 'F', '987123456')
+		  ,('E13', 'Markus', 'Keller', 'Guest Services Representative', 'G', 'DSV Basis-Instructor', 'Bachweg', '15', '87561', '1987-07-30', 34, 'M', '123789456')
+		  ,('E14', 'Melanie', 'Schuster', 'Equipment Rental Technician', 'I', 'DSV Snowsport-Instructor', 'Fichtenweg', '14', '87561', '1990-12-03', 31, 'F', '456789321')
+		  ,('E15', 'Peter', 'Schwarz', 'Administrative Staff', 'I', 'DSV Snowsport-Instructor', 'Lindenstraße', '13', '87561', '1985-09-17', 36, 'M', '789321654');
