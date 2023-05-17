a51db2eddf2be6ad674cfef579d0a50fbc0cd7a2
Jan Mühlnikel <jan.muehlnikel@gizonline.onmicrosoft.com>
2023-05-17 14:32:49 +0200

    added a venv

---
+++ APPLICATION/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, url_for, render_template, session, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+import os
