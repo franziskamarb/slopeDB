3a6549cb6b0b74f8a41ebc1f80003b8bf2b6c47c
AalliiM <120115598+AalliiM@users.noreply.github.com>
2023-05-17 13:36:02 +0200

    Update Insert_NN_Relation_Course_Student.sql

--- sql/insert_data/Insert_NN_Relation_Course_Student.sql
+++ sql/insert_data/Insert_NN_Relation_Course_Student.sql
@@ -1,70 +1,70 @@
 INSERT INTO COURSE_STUDENT
-	VALUES ('S1', 'Dec_I')
-		  ,('S2', 'Dec_I')
-		  ,('S3', 'Dec_I')
-		  ,('S4', 'Dec_II') 		  
-		  ,('S5', 'Dec_II')
-		  ,('S5', 'Jan_II')
-		  ,('S6', 'Dec_I')
-		  ,('S6', 'Jan_I')
-		  ,('S7', 'Jan_II') 
-		  ,('S8', 'Feb_I')
-		  ,('S9', 'Jan_I')
-		  ,('S10', 'Feb_I')	  
-		  ,('S11', 'Mar_II')
-		  ,('S12', 'Jan_III')
-		  ,('S13', 'Mar_I')
-		  ,('S14', 'Jan_II')
-		  ,('S14', 'Feb_II')
-		  ,('S15', 'Feb_III')
-		  ,('S16', 'Apr_I')	  
-		  ,('S17', 'Dec_I')
-		  ,('S18', 'Mar_I')
-		  ,('S19', 'Jan_I')	  
-		  ,('S20', 'Dec_II')
-		  ,('S20', 'Jan_II')
-		  ,('S21', 'Feb_I')
-		  ,('S22', 'Mar_I')
-		  ,('S23', 'Feb_III')
-		  ,('S23', 'Mar_III')
-		  ,('S24', 'Jan_I')
-		  ,('S25', 'Feb_I')
-		  ,('S25', 'Mar_I')
-		  ,('S26', 'Feb_II')
-		  ,('S27', 'Apr_III')
-		  ,('S28', 'Jan_I')
-		  ,('S29', 'Dec_II')
-		  ,('S29', 'Jan_II')
-		  ,('S30', 'Feb_III')
-		  ,('S31', 'Mar_I')  
-		  ,('S32', 'Mar_II')
-		  ,('S32', 'Apr_II')
-		  ,('S33', 'Jan_I')
-		  ,('S34', 'Jan_II')
-		  ,('S35', 'Jan_III')
-		  ,('S36', 'Jan_I')
-		  ,('S37', 'Jan_II')
-		  ,('S38', 'Feb_I')
-		  ,('S39', 'Feb_III') 
-		  ,('S40', 'Feb_II')  
-		  ,('S41', 'Feb_I')
-		  ,('S42', 'Feb_II')
-		  ,('S43', 'Mar_I')
-		  ,('S44', 'Feb_II')
-		  ,('S45', 'Apr_II')
-		  ,('S46', 'Jan_I')
-		  ,('S47', 'Mar_I')
-		  ,('S47', 'Apr_I')
-		  ,('S48', 'Mar_III')
-		  ,('S49', 'Jan_II')
-		  ,('S49', 'Feb_II')
-		  ,('S51', 'Mar_I')	
-		  ,('S52', 'Feb_I')  
-		  ,('S53', 'Jan_I')
-		  ,('S54', 'Mar_I')
-		  ,('S55', 'Mar_I')
-		  ,('S55', 'Apr_I')
-		  ,('S56', 'Feb_I')
-		  ,('S56', 'Mar_I')
-		  ,('S57', 'Jan_I')
-		  ,('S58', 'Mar_I');
+	VALUES ('Dec_I', 'S1'),
+('Dec_I', 'S2'),
+('Dec_I', 'S3'),
+('Dec_II', 'S4'),
+('Dec_II', 'S5'),
+('Jan_II', 'S5'),
+('Dec_I', 'S6'),
+('Jan_I', 'S6'),
+('Jan_II', 'S7'),
+('Feb_I', 'S8'),
+('Jan_I', 'S9'),
+('Feb_I', 'S10'),
+('Mar_II', 'S11'),
+('Jan_III', 'S12'),
+('Mar_I', 'S13'),
+('Jan_II', 'S14'),
+('Feb_II', 'S14'),
+('Feb_III', 'S15'),
+('Apr_I', 'S16'),
+('Dec_I', 'S17'),
+('Mar_I', 'S18'),
+('Jan_I', 'S19'),
+('Dec_II', 'S20'),
+('Jan_II', 'S20'),
+('Feb_I', 'S21'),
+('Mar_I', 'S22'),
+('Feb_III', 'S23'),
+('Mar_III', 'S23'),
+('Jan_I', 'S24'),
+('Feb_I', 'S25'),
+('Mar_I', 'S25'),
+('Feb_II', 'S26'),
+('Apr_III', 'S27'),
+('Jan_I', 'S28'),
+('Dec_II', 'S29'),
+('Jan_II', 'S29'),
+('Feb_III', 'S30'),
+('Mar_I', 'S31'),
+('Mar_II', 'S32'),
+('Apr_II', 'S32'),
+('Jan_I', 'S33'),
+('Jan_II', 'S34'),
+('Jan_III', 'S35'),
+('Jan_I', 'S36'),
+('Jan_II', 'S37'),
+('Feb_I', 'S38'),
+('Feb_III', 'S39'),
+('Feb_II', 'S40'),
+('Feb_I', 'S41'),
+('Feb_II', 'S42'),
+('Mar_I', 'S43'),
+('Feb_II', 'S44'),
+('Apr_II', 'S45'),
+('Jan_I', 'S46'),
+('Mar_I', 'S47'),
+('Apr_I', 'S47'),
+('Mar_III', 'S48'),
+('Jan_II', 'S49'),
+('Feb_II', 'S49'),
+('Mar_I', 'S51'),
+('Feb_I', 'S52'),
+('Jan_I', 'S53'),
+('Mar_I', 'S54'),
+('Mar_I', 'S55'),
+('Apr_I', 'S55'),
+('Feb_I', 'S56'),
+('Mar_I', 'S56'),
+('Jan_I', 'S57'),
+('Mar_I', 'S58');
