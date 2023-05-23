2de995581b3f7138f99fd397a265b3b589428c69
Jan Mühlnikel <jan.muehlnikel@gizonline.onmicrosoft.com>
2023-05-23 15:47:57 +0200

    added 1:1 selection wtf form

--- APPLICATION/app.py
+++ APPLICATION/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, url_for, render_template, session, redirect, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField, DateField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
 import os
@@ -12,21 +12,95 @@ bootstrap = Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://postgres:1234@database:5432/slopeDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 db = SQLAlchemy(app)
 
-from models import accomodation
+from models import Student, Accomodation, Ski
 
 app.app_context().push()
 db.create_all()
 
 @app.route('/')
 def index():
-    title="List of employees:"
-    employees = accomodation.query.order_by(accomodation.name.desc()).all()
-    return render_template('accomodations.html', employees=employees)
+    title="landing page"
+    return render_template('landing_page.html')
+
+@app.route('/students', methods=['GET'])
+def students():
+    students = Student.query.order_by(Student.last_name.asc()).all()
+
+    return render_template('students.html', students=students)
 
 
-#@app.route('/test')
-#def index():
-#    return f"results: {len(accomodation.query.order_by(accomodation.name).all())}"
+@app.route('/add_student', methods=['GET', 'POST'])
+def add_student():
+    form = AddStudentForm()
+    if form.validate_on_submit():
+        student = Student(
+            student_id="test_id",
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            birthdate="2005-05-21",
+            adult=True,
+            course_type="",
+            experience_level="",
+            phonenumber="",
+            sex="",
+            street="",
+            house_num="",
+            city="",
+            country="",
+            post_code=11111,
+            accomodation="A1",
+            ski_id="SM60",
+            helmet_id="H60",
+            pole_id="P60",
+            arrival_date="2005-05-21",
+            departure_date="2005-05-21"
+        )
+        db.session.add(student)
+        db.session.commit()
+        session['known'] = False
+        session['name'] = form.first_name.data
+        return redirect(url_for('index'))
+
+    return render_template('add_student.html', form=form)
+
+#Forms
+class AddStudentForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired()])
+    last_name = StringField('Last name', validators=[DataRequired()])
+    birthdate = StringField('Birthdate', validators=[DataRequired()])
+    course_type = SelectField('Course Type',
+                               choices=[('Ski', 'Ski'), ('Snowboard', 'Snowboard')],
+                                 validators=[DataRequired()])
+    experience_level = SelectField('Experience Level', 
+                                   choices=[('beginner', 'Beginner'), ('advanced', 'Advanced'), ('expert', 'Expert')],
+                                   validators=[DataRequired()])
+    phonenumber = StringField('Phonenumber', validators=[DataRequired()])
+    sex = SelectField('Sex', choices=[('male', 'Male'), ('female', 'Female')], validators=[DataRequired()])
+    street = StringField('Street', validators=[DataRequired()])
+    house_num = StringField('House Number', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    country = StringField('Country', validators=[DataRequired()])
+    post_code = StringField('Post Code', validators=[DataRequired()])
+    arrival_date = DateField('Arrival', validators=[DataRequired()])
+    departure_date = DateField('Departure', validators=[DataRequired()])  
+    accomodation = SelectField('Accomodation', 
+                               choices = 
+                               [
+                                   (accomodation.accomodation_id, f"{accomodation.name} - {accomodation.city}") 
+                                    for accomodation in Accomodation.query.all()
+                                ], 
+                                validators=[DataRequired()])
+    subquery = db.session.query(Student.ski_id).subquery()
+    ski = SelectField('Ski',
+                      choices = 
+                      [
+                          (ski.ski_id, f"({ski.ski_id}) {ski.brand} - {ski.modell} - Size: {ski.length}")
+                           for ski in Ski.query.filter(~Ski.ski_id.in_(subquery)).all()
+                        ], 
+                        validators=[DataRequired()])
+    
+    submit = SubmitField('Submit')
--- APPLICATION/models.py
+++ APPLICATION/models.py
@@ -2,15 +2,143 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from app import db
 
+class Salary(db.Model):
+    __tablename__ = 'salary'
+
+    salary_group = db.Column(db.String, primary_key=True)
+    salary = db.Column(db.BigInteger)
 
 
-class accomodation(db.Model):
+class Employee(db.Model):
+    __tablename__ = 'employee'
+
+    employee_id = db.Column(db.String, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    job_title = db.Column(db.String)
+    salary_group = db.Column(db.String, db.ForeignKey('salary.salary_group'))
+    instructor_level = db.Column(db.String)
+    street = db.Column(db.String)
+    house_num = db.Column(db.String)
+    city = db.Column(db.String)
+    country = db.Column(db.String)
+    post_code = db.Column(db.String)
+    birthdate = db.Column(db.Date)
+    age = db.Column(db.BigInteger)
+    sex = db.Column(db.String)
+    phonenumber = db.Column(db.String)
+    salary = db.relationship('Salary')
+
+
+class Accomodation(db.Model):
     __tablename__ = 'accomodation'
-    accomodation_id = db.Column('accomodation_id', db.String(64), primary_key = True)
-    name = db.Column(db.String(100))
-    type = db.Column(db.String(100))
-    street = db.Column(db.String(100))
-    house_num = db.Column(db.String(100))
-    city = db.Column(db.String(100))
-    country = db.Column(db.String(100))
-    post_code = db.Column(db.String(100))
+
+    accomodation_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String)
+    type = db.Column(db.String)
+    street = db.Column(db.String)
+    house_num = db.Column(db.String)
+    city = db.Column(db.String)
+    country = db.Column(db.String)
+    post_code = db.Column(db.String)
+
+
+class Ski(db.Model):
+    __tablename__ = 'ski'
+
+    ski_id = db.Column(db.String, primary_key=True)
+    modell = db.Column(db.String)
+    brand = db.Column(db.String)
+    length = db.Column(db.String)
+
+
+class Helmet(db.Model):
+    __tablename__ = 'helmets'
+
+    helmet_id = db.Column(db.String, primary_key=True)
+    brand = db.Column(db.String)
+    size = db.Column(db.String)
+
+
+class Pole(db.Model):
+    __tablename__ = 'poles'
+
+    poles_id = db.Column(db.String, primary_key=True)
+    brand = db.Column(db.String)
+    length = db.Column(db.String)
+
+
+class Student(db.Model):
+    __tablename__ = 'student'
+
+    student_id = db.Column(db.String, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    birthdate = db.Column(db.Date)
+    adult = db.Column(db.Boolean)
+    course_type = db.Column(db.String)
+    experience_level = db.Column(db.String)
+    phonenumber = db.Column(db.String)
+    sex = db.Column(db.String)
+    street = db.Column(db.String)
+    house_num = db.Column(db.String)
+    city = db.Column(db.String)
+    country = db.Column(db.String)
+    post_code = db.Column(db.BigInteger)
+    accomodation = db.Column(db.String, db.ForeignKey('accomodation.accomodation_id'))
+    ski_id = db.Column(db.String, db.ForeignKey('ski.ski_id'), unique=True)
+    helmet_id = db.Column(db.String, db.ForeignKey('helmets.helmet_id'), unique=True)
+    pole_id = db.Column(db.String, db.ForeignKey('poles.poles_id'), unique=True)
+    arrival_date = db.Column(db.Date)
+    departure_date = db.Column(db.Date)
+    accomodation_obj = db.relationship('Accomodation')
+    ski = db.relationship('Ski')
+    helmet = db.relationship('Helmet')
+    pole = db.relationship('Pole')
+
+
+class Area(db.Model):
+    __tablename__ = 'area'
+
+    area_id = db.Column(db.String, primary_key=True)
+    country = db.Column(db.String)
+    city = db.Column(db.String)
+    name = db.Column(db.String)
+    difficulty_level = db.Column(db.String)
+    opening_time = db.Column(db.Time)
+    closing_time = db.Column(db.Time)
+    magic_carpet = db.Column(db.Boolean)
+
+
+class Shuttle(db.Model):
+    __tablename__ = 'shuttle'
+
+    shuttle_name = db.Column(db.String, primary_key=True)
+    type = db.Column(db.String)
+    start_location = db.Column(db.String)
+    end_location = db.Column(db.String)
+    capacity = db.Column(db.BigInteger)
+    area_id = db.Column(db.String, db.ForeignKey('area.area_id'))
+    area = db.relationship('Area')
+
+
+class Course(db.Model):
+    __tablename__ = 'course'
+
+    course_id = db.Column(db.String, primary_key=True)
+    course_level = db.Column(db.String)
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    area = db.Column(db.String, db.ForeignKey('area.area_id'))
+    employee_id = db.Column(db.String, db.ForeignKey('employee.employee_id'))
+    area_obj = db.relationship('Area')
+    employee = db.relationship('Employee')
+
+
+class CourseStudent(db.Model):
+    __tablename__ = 'course_student'
+
+    course_course_id = db.Column(db.String, db.ForeignKey('course.course_id'), primary_key=True)
+    student_student_id = db.Column(db.String, db.ForeignKey('student.student_id'), primary_key=True)
+    course = db.relationship('Course')
+    student = db.relationship('Student')
